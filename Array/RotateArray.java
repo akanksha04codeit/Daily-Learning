237fa1aaf56c9514386fc065769da1060248aefb
Akanksha Pawashe <akankshapawashe@gmail.com>
2026-01-26 18:30:48 +0530

    Rotate Array by K Positions (Without Extra Space)

---
+++ Array/RotateArray.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class RotateArray {
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start++] = arr[end];
+            arr[end--] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        int k = 3;
+        k = k % arr.length;
+
+        reverse(arr, 0, arr.length-1);
+        reverse(arr, 0, k-1);
+        reverse(arr, k, arr.length-1);
+
+        for (int i : arr) System.out.print(i + " ");
+    }
+}
+
