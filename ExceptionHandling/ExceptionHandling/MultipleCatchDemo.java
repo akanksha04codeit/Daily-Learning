72b2989a1bda6f14b63c46951e6e3d296943a8bd
Akanksha Pawashe <akankshapawashe@gmail.com>
2026-01-29 15:23:53 +0530

    Multiple Catch Blocks (Arithmetic + Array)

---
+++ ExceptionHandling/ExceptionHandling/MultipleCatchDemo.java
@@ -0,0 +1,21 @@
+package ExceptionHandling;
+
+public class MultipleCatchDemo {
+    public static void main(String[] args) {
+
+        try {
+            int a = 10 / 0; // ArithmeticException
+
+            int[] arr = {1,2,3};
+            System.out.println(arr[5]); // ArrayIndexOutOfBounds
+
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero!");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index is invalid!");
+        }
+
+        System.out.println("Program continues...");
+    }
+}
+
